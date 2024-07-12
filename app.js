ba95cad025be6401af1f85258022ec0bfd082a7b
Suyoung <144405431+bellaajang@users.noreply.github.com>
2024-07-12 04:47:59 -0600

    navbar done! with animation using javascript

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
 
+//When you click the menu, the function will run.
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
